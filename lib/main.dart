4f8ccd63d265827ae84f4b12183e5233a2f5126a
Wasiue03 <channarwasiue@yahoo.com>
2023-09-02 14:38:02 -0700

    files arranged

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:rentatouille/services/notification.dart';
+import 'package:rentatouille/services/Notifications/notification.dart';
 import 'package:rentatouille/views/renter_screens/renter_homepage.dart';
-import 'package:rentatouille/views/landing_page.dart';
-import 'package:rentatouille/views/login.dart';
+import 'package:rentatouille/views/Landing/landing_page.dart';
+import 'package:rentatouille/views/Login/login.dart';
 import 'package:rentatouille/views/seller_screens/seller_homepage.dart';
 
 void main() async {
@@ -15,6 +14,8 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Your App',
       initialRoute: '/Landing', // Set the initial route
       routes: {
-        '/Landing': (context) => LandingScreen(),
+        '/Landing': (context) => const LandingScreen(),
         '/login': (context) => LoginPage(),
         '/renterHomepage': (context) => RenterHomeScreen(),
         '/sellerHomepage': (context) => SellerHomeScreen(),
